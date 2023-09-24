cdc8131c56fa9037725d26291222720e9b70389e
Duong <nguyenthaiduongdannie@gmail.com>
2023-09-24 19:44:52 +0700

    'finished_addingNum'

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var mainArray = [];
 document.getElementById('add_number_btn').onclick = function () {
     var addNum = document.getElementById('add_number').value;
+    if (addNum === "") {
+        mainArray.push(0);
+    } else {
+        mainArray.push(addNum);
+    }
 
-    mainArray.push(addNum);
     document.getElementById('array_number').innerHTML = mainArray;
 
 }
