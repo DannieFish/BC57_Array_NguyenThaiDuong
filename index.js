508a8d0dfe4cd3de6dc46e52b6507fcd2ae0ba22
Duong <nguyenthaiduongdannie@gmail.com>
2023-09-24 21:07:57 +0700

    'finished_sum_positive'

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var mainArray = [];
+
 document.getElementById('add_number_btn').onclick = function () {
-    var addNum = document.getElementById('add_number').value;
+    var addNum = +document.getElementById('add_number').value;
     if (addNum === "") {
         mainArray.push(0);
     } else {
@@ -10,3 +11,14 @@ document.getElementById('add_number_btn').onclick = function () {
     document.getElementById('array_number').innerHTML = mainArray;
 
 }
+
+document.getElementById('sum_positive').onclick = function () {
+    var sumPositive = 0;
+    for (var index = 0; index < mainArray.length; index++) {
+        if (mainArray[index] > 0) {
+            sumPositive += mainArray[index];
+        }
+    }
+    document.getElementById('res_1').innerHTML = sumPositive;
+}
+
